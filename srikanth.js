2ba602cfc9e9af072c41dedec5365b8752d55204
srikanth <srikanthreddyvr1@gmail.com>
2016-03-18 15:29:34 -0500

    changes

--- srikanth.js
+++ srikanth.js
@@ -7,12 +7,12 @@ function person(first, last, age, eye) {
     this.age = age;
     this.eyeColor = eye;
     this.name = function() {
-        return this.firstName + " " + this.lastName +""+this.age+""+this.eyeColor
+        return this.firstName + " " + this.lastName + " " +this.age+ " " +this.eyeColor
     };
 }
 person.prototype.nationality = "English";
 
-var sri = new person("sriaknth", "sanikommu", 25, "green");
+var sri = new person("srikanth", "sanikommu", 25, "green");
 var kanth = new person("sanikommu", "sriaknthreddy", 25, "blue");
 document.getElementById("reddy");
 innerHTML = " sri is " + sri.eyeColor + " kanth is " + kanth.age;
